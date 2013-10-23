cdfab31f795c9331564448651abfc3b34ef26a36
Simon Cropp <simon.cropp@gmail.com>
2013-10-23 17:39:02 +1100

    remove build reference

--- Tests/Verifier.cs
+++ Tests/Verifier.cs
@@ -1,30 +1,50 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
-using Microsoft.Build.Utilities;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 public static class Verifier
 {
-    public static void Verify(string assemblyPath2)
+    public static void Verify(string beforeAssemblyPath, string afterAssemblyPath)
     {
-
-        var exePath = GetPathToPEVerify();        
-        var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
-                                        {
-                                            RedirectStandardOutput = true,
-                                            UseShellExecute = false,
-                                            CreateNoWindow = true
-                                        });
-
-        process.WaitForExit(10000);
-        var readToEnd = process.StandardOutput.ReadToEnd().Trim();
-        Assert.IsTrue(readToEnd.Contains(string.Format("All Classes and Methods in {0} Verified.", assemblyPath2)), readToEnd);
+        var before = Validate(beforeAssemblyPath);
+        var after = Validate(afterAssemblyPath);
+        var message = string.Format("Failed processing {0}\r\n{1}", Path.GetFileName(afterAssemblyPath), after);
+        Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
     }
 
-    private static string GetPathToPEVerify()
+    static string Validate(string assemblyPath2)
     {
-        var peverifyPath = Path.Combine(
-            ToolLocationHelper.GetPathToDotNetFrameworkSdk(TargetDotNetFrameworkVersion.Version40), @"bin\NETFX 4.0 Tools\peverify.exe");
-        return peverifyPath;
+        var exePath = GetPathToPEVerify();
+        if (!File.Exists(exePath))
+        {
+            return string.Empty;
+        }
+        var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        });
+
+        process.WaitForExit(10000);
+        return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
+    }
+
+    static string GetPathToPEVerify()
+    {
+        var exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
+
+        if (!File.Exists(exePath))
+        {
+            exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
+        }
+        return exePath;
+    }
+
+    static string TrimLineNumbers(string foo)
+    {
+        return Regex.Replace(foo, @"0x.*]", "");
     }
 }
--- Tests/WeaverTests.cs
+++ Tests/WeaverTests.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
+using Mono.Cecil;
 using NUnit.Framework;
 
 [TestFixture]
 public class WeaverTests
 {
     Assembly assembly;
+    string newAssemblyPath;
+    string assemblyPath;
 
     [TestFixtureSetUp]
     public void Setup()
     {
-        assembly = WeaverHelper.WeaveAssembly();
+        var projectPath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\AssemblyToProcess\AssemblyToProcess.csproj"));
+        assemblyPath = Path.Combine(Path.GetDirectoryName(projectPath), @"bin\Debug\AssemblyToProcess.dll");
+#if (!DEBUG)
+        assemblyPath = assemblyPath.Replace("Debug", "Release");
+#endif
+
+        newAssemblyPath = assemblyPath.Replace(".dll", "2.dll");
+        File.Copy(assemblyPath, newAssemblyPath, true);
+
+        var moduleDefinition = ModuleDefinition.ReadModule(newAssemblyPath);
+        var weavingTask = new ModuleWeaver
+        {
+            ModuleDefinition = moduleDefinition
+        };
+
+        weavingTask.Execute();
+        moduleDefinition.Write(newAssemblyPath);
+
+        assembly = Assembly.LoadFile(newAssemblyPath);
     }
 
     [Test]
@@ -26,8 +48,7 @@ public class WeaverTests
     [Test]
     public void PeVerify()
     {
-        Verifier.Verify(assembly.CodeBase.Remove(0, 8));
+        Verifier.Verify(assemblyPath,newAssemblyPath);
     }
 #endif
-
 }
