2f947b0ce874802da94a28195f51342acfd12efd
Andrea Canciani <ranma42@gmail.com>
2015-06-22 12:23:56 +0200

    Update metadata
    Update project, assembly and nuget package metadata.

--- ProtectMe/AssemblyInfo.cs
+++ ProtectMe/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("SampleFodyAddin")]
-[assembly: AssemblyProduct("SampleFodyAddin")]
-[assembly: AssemblyVersion("0.3.2")]
-[assembly: AssemblyFileVersion("0.3.2")]
+[assembly: AssemblyTitle("ProtectMe")]
+[assembly: AssemblyProduct("ProtectMe")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
